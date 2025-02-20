da06176625c1c48df37772d0c5de5b06666d4ca6
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2025-02-20 10:14:51 +0530

    template added

--- spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/test/java/com/app/ApplicationTests.java
+++ spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/test/java/com/app/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.blogs;
+package com.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
