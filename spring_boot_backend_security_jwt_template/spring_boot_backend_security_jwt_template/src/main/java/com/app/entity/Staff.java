6150463a54b854297f303486ed2555ddbf16c949
poojaJ214 <technicalupgradationhub@gmail.com>
2025-02-20 10:37:57 +0530

    staff entity done

---
+++ spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/main/java/com/app/entity/Staff.java
@@ -0,0 +1,97 @@
+package com.app.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Staff {
+
+	@Id
+	private String userid;
+	private String uname;
+	private String pwd;
+	private String phone;
+	private String gender;
+	private String address;
+	private String staffid;
+	private LocalDateTime createdon;
+	
+	public Staff() {
+		this.createdon=LocalDateTime.now();
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getStaffid() {
+		return staffid;
+	}
+
+	public void setStaffid(String staffid) {
+		this.staffid = staffid;
+	}
+
+	public LocalDateTime getCreatedon() {
+		return createdon;
+	}
+
+	public void setCreatedon(LocalDateTime createdon) {
+		this.createdon = createdon;
+	}
+
+	@Override
+	public String toString() {
+		return "Staff [userid=" + userid + ", uname=" + uname + ", pwd=" + pwd + ", phone=" + phone + ", gender="
+				+ gender + ", address=" + address + ", staffid=" + staffid + ", createdon=" + createdon + "]";
+	}
+	
+	
+}
+
